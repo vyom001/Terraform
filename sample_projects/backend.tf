8ab3cde0badf806af248c479b64870edfbffac31
VYOM KUMAR <39191783+vyom001@users.noreply.github.com>
2024-09-10 20:24:01 +0530

    Update backend.tf

--- sample_projects/backend.tf
+++ sample_projects/backend.tf
@@ -1,1 +1,11 @@
+terraform {
+  backend "s3" {
+    bucket         = "vyom1607200016072000"
+    key            = "sample_projects/terraform.tfstate"
+    region         = "us-east-1"
+    encrypt        = true
+  }
+}
 
+
+#s3 bucket must be created already
