b836e1af23d2839557dbbdfcde475525e3509b94
VYOM KUMAR <39191783+vyom001@users.noreply.github.com>
2024-09-10 19:20:03 +0530

    Update main.tf

--- sample_projects/Demo_ec2-module/main.tf
+++ sample_projects/Demo_ec2-module/main.tf
@@ -1,8 +1,10 @@
 resource "aws_instance" "example" {
-    ami = var.ami_name
-    instance_type = var.instance_type
-    count = 5 #5 ec2 instances will be launched
+    count= 3
+    ami= var.ami_name
+    instance_type = var.instance_size
+
     tags = {
-    Name = "instance_number-${count.index} #assign name to the instances from 0-4
+        Name = "instance_number-${count.index}"
     }
 }
+
