4bb1536f8b7267b2b2548a6cf2e590999c9aa457
VYOM KUMAR <39191783+vyom001@users.noreply.github.com>
2024-09-10 19:39:10 +0530

    Update main.tf

--- sample_projects/main.tf
+++ sample_projects/main.tf
@@ -1,4 +1,4 @@
 module "ec2_instance_module" {
   source = "./Demo_ec2_module"
-  ami = var.ami_name
-  instance_type = var.instance_type
+  ami_name = var.ami_name
+  instance_size = var.instance_size
