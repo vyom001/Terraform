fa5e0bbf53f17f4554e0c7a7623fe39152a68650
VYOM KUMAR <39191783+vyom001@users.noreply.github.com>
2024-09-10 20:23:20 +0530

    Update main.tf

--- sample_projects/main.tf
+++ sample_projects/main.tf
@@ -3,3 +3,8 @@ module "ec2_instance_module" {
   ami_name = var.ami_name
   instance_size = var.instance_size
 }
+
+#s3 bucket must be created already
+resource "aws_s3_bucket" "s3_bucket" {
+  bucket = "vyom1607200016072000"  
+}
