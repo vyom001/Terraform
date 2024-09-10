00df5261bf9da9a22a7465261822c56fbc9cc016
VYOM KUMAR <39191783+vyom001@users.noreply.github.com>
2024-09-10 18:28:58 +0530

    Update main.tf

--- sample_projects/main.tf
+++ sample_projects/main.tf
@@ -1,4 +1,4 @@
 module "ec2_instance_module" {
   source = "./Demo_ec2_module"
-  ami_value = "ami-053b0d53c279acc90" 
-  instance_type_value = "t2.micro"
+  ami = var.ami_name
+  instance_type = var.instance_type
